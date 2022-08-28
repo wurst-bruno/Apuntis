a444399af0cced3d564ea67117bbd4113b1f6c95
wurst-bruno <bruno.wurstt@gmail.com>
2022-08-28 19:19:56 -0300

    Update Apuntis.cs

--- Assets/Apuntis.cs
+++ Assets/Apuntis.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // engine para cambiar de escena
+using UnityEngine.UI; //UI engine
+
+
 
 public class Apuntis : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //1.Crear array publico de game objects
+    public GameObject[] arrayDeMesas;
+
+
     void Start()
     {
-        
+        // 2.asignar todos los objetos taggeados como "Mesa" al array
+        arrayDeMesas = GameObject.FindGameObjectsWithTag("Mesa");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Invocar la funcion al tocar una tecla.
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            DesactivateObjectsInArray();
+            DestroyDestructible();
+        }
+    }
+
+    // 3. crear una función que desactive todos los elementos del array 
+    void DesactivateObjectsInArray()
+    {
+        for (int i = 0; i < arrayDeMesas.Length; i++)
+        {
+            arrayDeMesas[i].SetActive(false);
+        }
+    }
+
+    //5.Crear una funcion que asigne todos los elementos del array el script mesa;
+    //Establecer el valor de la variable "destructible" aleatoriamente.
+
+    void AssignScriptMesaToArrayElement()
+    {
+        foreach (GameObject go in arrayDeMesas)
+        {
+            go.AddComponent<Mesa>();
+            go.GetComponent<Mesa>().destructible = Random.Range(0, 2) == 0;
+
+        }
+    }
+
+
+
+
+
+
+    //6. Crear una funcion que destruya el elemento del array que contenga un
+    //script "Mesa" cuya variable booleana "Destructible" sea true. (Crear el script "Mesa")
+
+
+    void DestroyDestructible()
+    {
+        foreach (GameObject go in arrayDeMesas)
+        {
+            if (go.GetComponent<Mesa>().destructible)
+            {
+                Destroy(go);
+            }
+        }
     }
 }
