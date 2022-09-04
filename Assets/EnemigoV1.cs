eab52547fd4fb0f796cf8cd65234b47122315a2b
wurst-bruno <bruno.wurstt@gmail.com>
2022-09-04 13:54:21 -0300

    Hola

---
+++ Assets/EnemigoV1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoV1 : MonoBehaviour
+{
+    public bool defeated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown() //https://drive.google.com/drive/folders/1o1MtamPfRG1G1SvO_D3__VnRIE6f-Bk8 de la izq minuto 4.30
+    {
+        defeated = true;
+        GetComponent<MeshRenderer>().enabled = false; //Desactiva el MR
+        GetComponent<Collider>().enabled = false; //Desactiva el Collider, no importa cual(cube, circle, etc) podes asignar todo eso a cada elemtno no importa forma ni nada.
+
+    }
+}
---
+++ Assets/GameSceneManager.cs
@@ -0,0 +1,27 @@
+﻿//Esto es para el boton y crean un empty y ponerle este script
+//https://drive.google.com/drive/folders/1o1MtamPfRG1G1SvO_D3__VnRIE6f-Bk8 derecha 11:45
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoToScene(string sceneName) //le pónes el nombre de la escena en la scene cuando le decis al boton que hacer
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
